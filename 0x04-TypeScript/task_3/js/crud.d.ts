77f1f26450f511687d53de695366b0a00137ac27
mansouriaminepro-1 <mansouriamine.pro@gmail.com>
2025-10-19 14:12:31 +0000

    Refactor CRUD function declarations in crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -8,6 +8,8 @@ export interface RowElement {
 
 import { RowId , RowElement} from "./interface"
 
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(row : RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
+//export function insertRow(row: RowElement): RowID;
+//export function deleteRow(row : RowID): void;
+declare function deleteRow(rowId: RowID): void
+declare function updateRow(RowElement,RowID): RowID;
